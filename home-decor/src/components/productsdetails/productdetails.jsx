082a7c42d8d203a268b3b45954b4222ec148646c
singh2198 <8755akash@gmail.com>
2023-01-18 22:23:17 +0530

    day_2_akash

--- home-decor/src/components/productsdetails/productdetails.jsx
+++ home-decor/src/components/productsdetails/productdetails.jsx
@@ -1,34 +1,10 @@
 import React from "react";
-import { useEffect, useState } from "react";
 import Sideimage from "./sideimage";
 import Bigimage from "./Bigimage";
 import Cartitems from "./Cartitems";
 import Discreption from "./Discreption";
 
 function Productdetails() {
-  const [productdata, setproductdata] = useState([]);
-
-  const getapidata = async () => {
-    try {
-      let res = await fetch(
-        `https://mock-server-ge69.onrender.com/api/Products/1`
-      ).then((res) => res);
-      let data = await res.json();
-      setproductdata(data);
-      //   return data;
-      //   console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-
-    // return response.data
-  };
-
-  useEffect(() => {
-    getapidata();
-  }, []);
-
-  //   console.log(productdata);
   return (
     <div>
       <div

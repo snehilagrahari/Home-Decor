0de86dfca90418b2a035b6e3790a9337f89b688f
singh2198 <8755akash@gmail.com>
2023-01-20 16:00:33 +0530

    final@day_4 akash

--- home-decor/src/components/productsdetails/Bigimage.jsx
+++ home-decor/src/components/productsdetails/Bigimage.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Image } from "@chakra-ui/react";
 
-function Bigimage({ images }) {
+function Bigimage(props) {
   // console.log(images[0]);
+  let {images}=props
+  // const images[0] =images
+  console.log(images);
+  // let i = 0;
+  // let valu=""
+  // for (i in images) {
+  //   valu = images[i];
+  //   return;
+  // }
+  // console.log(valu);
+
   return (
     <>
       <div
@@ -13,12 +24,7 @@ function Bigimage({ images }) {
           left: "-62%",
         }}
       >
-        <Image
-          boxSize="90%"
-          objectFit="cover"
-          src={images[0]}
-          alt="Dan Abramov"
-        />
+        <Image boxSize="90%" objectFit="cover" src={images[0]} alt="Dan Abramov" />
       </div>
     </>
   );
